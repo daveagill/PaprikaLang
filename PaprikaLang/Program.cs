82cc5111a03c67f643ef05265b8fd6a668c3b507
David Gill <davegill88@gmail.com>
2016-12-14 22:49:50 +0000

    Add exception handling and logging for compile-time errors

--- PaprikaLang/Program.cs
+++ PaprikaLang/Program.cs
@@ -9,22 +9,33 @@ namespace PaprikaLang
 	{
 		public static void Main(string[] args)
 		{
-			string sourceFilePath = "paprika scripts/program.paprika";
-			if (args.Length > 0)
+			try
 			{
-				sourceFilePath = args[0];
+				string sourceFilePath = "paprika scripts/program.paprika";
+				if (args.Length > 0)
+				{
+					sourceFilePath = args[0];
+				}
+
+				string programName = Path.GetFileNameWithoutExtension(sourceFilePath);
+				string sourceCode = File.ReadAllText(sourceFilePath);
+
+				ASTModule module = Parser.Parse(sourceCode).RootNode;
+				SemanticAnalyser.Analyse(module);
+				Emitter.Emit(programName, programName + ".exe", module);
+
+				string asString = new ASTStringifier().Stringify(module);
+				Console.WriteLine(asString);
+				Console.WriteLine("\n\nCompilation Complete!");
 			}
+			catch (Exception e)
+			{
+				Console.Write("Compilation failed: ");
+				Console.WriteLine(e.Message);
 
-			string programName = Path.GetFileNameWithoutExtension(sourceFilePath);
-			string sourceCode = File.ReadAllText(sourceFilePath);
-
-			ASTModule module = Parser.Parse(sourceCode).RootNode;
-			SemanticAnalyser.Analyse(module);
-			Emitter.Emit(programName, programName + ".exe", module);
-
-			string asString = new ASTStringifier().Stringify(module);
-			Console.WriteLine(asString);
-			Console.WriteLine("\n\nCompilation Complete!");
+				Console.WriteLine("\n\nStack Trace:");
+				Console.WriteLine(e);
+			}
 		}
 	}
 }
